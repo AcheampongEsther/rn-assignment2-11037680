43729b4756d77711f09868d7fe1c9589af3c8817
Esther Acheampong <124188604+AcheampongEsther@users.noreply.github.com>
2024-05-26 19:33:54 -1200

    Add files via upload

---
+++ MyAssignment.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function MyAssignment() {
+  return (
+    <h1 style={{ backgroundColor: '#f0f0f0', flex: 1, justifyContent: 'center', alignItems: 'center' , fontSize: 24, fontWeight: 'bold' }}>My Name is Esther</h1>  
+  );
+}
